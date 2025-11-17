345c4e96788aac7b98c055e6ad1249d9c5ab65e2
kantomalala <kantoandriamanakasina@gmail.com>
2025-11-17 22:13:56 +0300

    sprint4
    oke

--- mety/src/test/urlAnnotations/UrlController.java
+++ mety/src/test/urlAnnotations/UrlController.java
@@ -8,11 +8,16 @@ public class UrlController {
 
     @Url("/test1")
     public String hello() {
-        return "hello";
+        return "Bonjour depuis le contrôleur ! Ceci est le Sprint 4";
     }
 
     @Url("/test2")
     public String bye() {
-        return "bye";
+        return "Au revoir ! La valeur retournée est affichée directement";
+    }
+
+    @Url("/test3")
+    public void classic() {
+        // Pas de return → affichage classique
     }
 }
